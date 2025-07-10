2a27efe6765bd1ab6c2ef495ba008fd394936feb
Oleh Shevchenko <oshevchenko@edu.cdv.pl>
2025-07-10 23:46:23 +0200

    0710 - Added Fishnet plugin to replace Unity NetCode

--- Assets/Scripts/Network/NetworkWorldStart.cs
+++
@@ -1,8 +0,0 @@
-using Unity.Netcode;
-using UnityEngine;
-
-public class NetworkWorldStart : MonoBehaviour
-{
-    public void StartHost() => NetworkManager.Singleton.StartHost();
-    public void StartClient() => NetworkManager.Singleton.StartClient();
-}
